73b79bb7ec122745c9dce73ef14700b98dc9c464
cleanical <miraclesunday72@gmail.com>
2023-09-09 09:07:22 -0700

    7-print_tebahpla file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ * Return: always
+ */
+
+int main(void)
+	{
+	int alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+
+	{
+	putchar (alph);
+	}
+	putchar ('\n');
+	return (0);
+		}
