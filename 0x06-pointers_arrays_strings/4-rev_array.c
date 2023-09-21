d54e6c829b7ddef851ca1326443415a947520f7e
cleanical <miraclesunday72@gmail.com>
2023-09-21 07:05:29 -0700

    4-rev_array file added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - function to reverse arrays
+ * @a: array
+ * @n: number of elements of the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; i++)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+
+}
