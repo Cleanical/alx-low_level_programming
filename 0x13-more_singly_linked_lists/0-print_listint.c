9e983c1db61c754bb5f78a6920686e2020d11fff
cleanical <miraclesunday72@gmail.com>
2023-10-23 21:21:10 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - function to print the int data in a asingly linked list
+ * @h: head pointer to the list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (count);
+
+}
