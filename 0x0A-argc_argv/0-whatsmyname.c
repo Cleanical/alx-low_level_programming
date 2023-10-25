f2e6ab4f971c74247852f0d99794e56e6cef2268
cleanical <miraclesunday72@gmail.com>
2023-10-25 11:46:51 -0700

    file mynameis added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is the main function
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguments
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
